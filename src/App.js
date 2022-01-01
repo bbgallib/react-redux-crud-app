26b2644a28773c2cfea173d312085a82858a096a
bbgallib <bbgallib@gmail.com>
2022-01-01 21:54:53 +0900

    create functinal component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-function App() {
-  // const greeting = "Hi, Tom!"
-  // const dom = <h1 className="foo">{greeting}</h1>
-  // return dom;
-
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked!")}}></input>
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
